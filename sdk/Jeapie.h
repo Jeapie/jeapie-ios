0ab82e73f313b7532c3cb1f43c7af6aa3cd6a73f
Shananin <cto@jeapie.com>
2014-03-06 18:50:57 +0200

    add sdk

---
+++ sdk/Jeapie.h
@@ -0,0 +1,21 @@
+//
+//  Jeapie.h
+//  Greenlamp
+//
+//  Created by Amoneron on 03.03.14.
+//  Copyright (c) 2014 Alexander Murzanaev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Jeapie : NSObject
+
++(void)startSessionWithKey:(NSString *)key secret:(NSString *)secret launchOptions:(NSDictionary *)launchOptions;
+
++(void)didReceiveRemoteNotification:(NSDictionary *)userInfo;
+
++(void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken;
+
++(void)tags:(NSArray *)tags;
+
+@end
